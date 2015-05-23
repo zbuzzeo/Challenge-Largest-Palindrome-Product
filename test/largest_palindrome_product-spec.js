f70f9f8c89de128f1525b6d0fbce747bbffd7bdc
sgnl <rayrfarias@gmail.com>
2015-05-23 10:29:58 -1000

    fix tests

--- test/largest_palindrome_product-spec.js
+++ test/largest_palindrome_product-spec.js
@@ -1,25 +1,28 @@
 var chai = require('chai');
 var expect = chai.expect;
-var findPalidromeProduct = require("../largest_palindrome_product.js");
+var findPalindromeProduct = require("../largest_palindrome_product.js");
 
 describe("The Largest Palindrome Product", function () {
-  it("of two 2 digit numbers should be 9009, 91×99", function () {\
+  it("of two 2 digit numbers should be 9009, 91 x 99", function () {
     var result = findPalindromeProduct(1,2);
 
     expect(result.palindromeNumber).to.equal(9009);
 
     // make sure solution contains the two factors
-    expect([91, 99]).to.have.members(result.factor_0);
-    expect([91, 99]).to.have.members(result.factor_1);
+    expect([91, 99]).to.include(result.factor_0);
+    expect([91, 99]).to.include(result.factor_1);
   });
 
   it("of two 3 digit numbers should be 906609", function () {
-    var product = challenge_4.largestPalindromeProduct(1,3);
+    var product = findPalindromeProduct(1,3);
 
-    expect(product.palindromeNumber).toEqual(906609);
+    expect(product.palindromeNumber).to.equal(906609);
 
     // add your tests here
     //
     //
+  });
+  it('should only accept Numbers as arugments', function () {
+
   });
 });
